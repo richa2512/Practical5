5ead400e754146f4a6d11d3b1d2cf8a2c75547d9
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:29:47 +0530

    List of files

---
+++ ListOfFiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practical5_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] Directories = Directory.GetDirectories(@"C:\Users\RICHA\source\repos");
+            Console.WriteLine("All the Directories are:");
+            foreach (string dir in Directories)
+            {
+                //Console.WriteLine("All the Directories are:");
+                Console.WriteLine(dir);
+            }
+            string[] files = Directory.GetFiles(@"C:\Users\RICHA\source\repos");
+            Console.WriteLine("All the Files are:");
+            foreach (string file in files)
+            {
+               // Console.WriteLine("All the Files are:");
+                Console.WriteLine(file);
+            }
+            Console.ReadLine();
+        }
+    }
+}
