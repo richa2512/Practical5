06c0d2f1b9f0c047ebd83f6832fe73321a645038
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:27:53 +0530

    Read to end of line

---
+++ ReadEndOfLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practical5_1_
+{
+    class Program
+    {
+      
+
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("teststream.txt");
+            using (reader)
+            {
+                int lineNumber = 0;
+                String line = reader.ReadLine();
+                while(line!=null)
+                {
+                    lineNumber++;
+                    Console.WriteLine("Line {0}:{1}", lineNumber, line);
+                    line = reader.ReadLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
