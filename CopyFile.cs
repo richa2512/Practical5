abe900f4acdec3541d86a492d2997152f1e408c0
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:28:49 +0530

    Copy from file1 to file2

---
+++ CopyFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practical5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyFile cp = new CopyFile();
+            String file1= @"C:\dotNet\file1.txt";
+            String file2 = @"C:\dotNet\richa\file2.txt";
+            cp.copyFile(file1, file2);      
+        }
+    }
+    public class CopyFile
+    {
+        public void copyFile(String file1,String file2)
+        {
+            using (StreamReader reader = new StreamReader(file1)) 
+            {
+                using (StreamWriter writer = new StreamWriter(file2))
+                {
+                    String line = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(line);
+
+                    }
+                }
+            }
+        }
+    }
+}
